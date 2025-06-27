9e7169ca30fdb4cedbf1b9901cae520dc57bc84e
Ritwik Chakradhar <ritwik.chakradhar@gmail.com>
2025-06-27 12:31:05 +0530

    Deploy static site to GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-module.exports = nextConfig;
+const nextConfig = {
+    output: 'export',
+    images: { unoptimized: true },
+    basePath: '/ritwik-portfolio', // replace with your GitHub repo name exactly
+  };
+  
+  module.exports = nextConfig;
+  
